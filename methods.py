3da56be37b575986dd483c42a61ee2766da14b23
trungkak <trungkak@gmail.com>
2018-06-18 16:02:42 +0700

    Add 19888

--- methods.py
+++ methods.py
@@ -5,53 +5,22 @@ from datetime import datetime
 from bs4 import BeautifulSoup, Comment
 
 
-def get_bet_odds(url_or_source, title):
-    tables = pd.read_html(url_or_source)
-    bet_table = tables[0]
-    bet_table.columns = ['winner', 'bet365', 'skybet',
-                         'ladbrokes', 'William Hill', 'Marathon Bet',
-                         'Betfair Sportsbook', 'SunBets',
-                         'Paddy Power', 'Unibet', 'Coral',
-                         'Betfred', 'Boylesports', 'Black Type',
-                         'Betstars', 'Betway', 'BetBright',
-                         '10Bet', 'Sportingbet', '188Bet',
-                         '888sport', 'Bet Victor', 'Sportpesa',
-                         'Spreadex', 'IGNORE_REDUNDANT_COLUMN', 'Betfair', 'Betdaq',
-                         'Matchbook', 'Smarkets',
-                         ]
-    result = {title: {}}
-
-    top_dealers = bet_table.dropna(axis=1)
-
-    for i, w in enumerate(top_dealers['winner']):
-        result[title][w] = []
-        for dealer in top_dealers.columns[1:].values.tolist():
-            result[title][w].append({dealer: top_dealers[dealer][i]})
-
-    return result
-
-
-def get_bet_ratio(source):
-    ratios = {}
-
-    for i in [1, 2, 3]:
-        try:
-            extracted_text = Selector(text=source) \
-                                .xpath('//*[@id="highcharts-0"]/svg/g[2]/g/g/g[{}]/text/tspan/text()'.format(str(i))) \
-                                .extract()[0]
-            team_name, ratio = extracted_text.split()[:-1][0], extracted_text.split()[-1].strip('(|)')
-            ratios[team_name] = ratio
-        except:
-            continue
-
-    return ratios
-
-
-def get_google_winner(source, title):
+def get_google_winner(driver, source, title):
     winning_percents = {}
     team_names = title.split('-v-')
     team1 = ' '.join(team_names[0].split('-'))
     team2 = ' '.join(team_names[1].split('-'))
+
+    # lis = driver.find_elements_by_tag_name('li')
+
+    # for li in lis:
+    #     if li.text == 'ĐỘI HÌNH RA SÂN':
+    #         print(li)
+    #         li.click()
+    #         break
+
+    # source = driver.page_source
+    soup = BeautifulSoup(source, "lxml")
 
     try:
         team1_percent = Selector(text=source) \
@@ -74,84 +43,61 @@ def get_google_winner(source, title):
     except:
         team2_percent = 'ERROR'
 
+    try:
+        match_score_div = soup.find('div', class_ = "imso_mh__ma-sc-cont")
+        match_score = ' '.join(match_score_div.find_all(text=True))[:3]
+    except:
+        match_score = 'ERROR'
+
+    try:
+        match_time_div = soup.find('div', class_ = "imso_mh__lv-m-stts-cont")
+        match_time = ' '.join(match_time_div.find_all(text=True))[:2].strip()
+    except:
+        if match_score != 'ERROR':
+            match_time = 'MATCHOVER'
+        else:
+            match_time = 'ERROR'
+
+    lineups_div = soup.find('div', class_ = "lineup-section")
+
+    team_lineup = {
+        'team1': {},
+        'team2': {}
+    }
+    if lineups_div:
+        try:
+            all_players = ' '.join([div.text for div in soup.find_all('span', class_="jersey-num-cell")])
+            team_lineup['team1']['players'] = all_players.split()[:11]
+            team_lineup['team2']['players'] = all_players.split()[11:]
+
+            print(team_lineup['team1']['players'])
+            print(team_lineup['team2']['players'])
+
+            all_formations = ' '.join(
+                [div.text for div in soup.find_all('span', class_="oEwJQ tBpa2 text-middle formation")])
+            print(all_formations)
+            team_lineup['team1']['lineup'] = all_formations.split()[0]
+            team_lineup['team2']['lineup'] = all_formations.split()[1]
+        except:
+            print('MATCH NOT STARTED')
+
     winning_percents[team1] = team1_percent
     winning_percents['draw'] = draw_percent
     winning_percents[team2] = team2_percent
     winning_percents['first_team'] = team1
     winning_percents['second_team'] = team2
 
+    if match_score != 'ERROR' and match_time != 'ERROR':
+        winning_percents['match_score'] = match_score
+        winning_percents['match_time'] = match_time
+
+    if team_lineup['team1'] != {}:
+        winning_percents['lineups'] = team_lineup
+
     return winning_percents
 
 
 # Keo nha cai data
-
-def concat_row_data(sub_table):
-    result = {}
-    names = ['team1', 'team2', 'draw']
-    for i, tr in enumerate(sub_table.find('tbody').find_all('tr', recursive=False)):
-        tds = tr.find_all('td')
-        content = [td.text for td in tds if td.text.strip() != '']
-        if len(content) != 0:
-            result[names[i]] = content
-    return result
-
-    # result = []
-    #
-    # for tr in sub_table.find('tbody').find_all('tr', recursive=False):
-    #     tds = tr.find_all('td')
-    #     result.append([td.text for td in tds])
-    # return result
-
-
-def parse_row(elem):
-    COLUMN_NAMES = ['DATETIME',
-                    'MATCHNAME',
-                    'CATRAN-TYLE',
-                    'CATRAN-TAIXIU',
-                    'CATRAN-1X2',
-                    'HIEP1-TYLE',
-                    'HIEP1-TAIXIU',
-                    'HIEP1-1X2']
-
-    match_obj = {}
-    tds = elem.find_all('td', recursive=False)
-
-    for index, name in enumerate(COLUMN_NAMES):
-        td = tds[index]
-        if len(td.find_all('table')) == 0:
-            match_obj[name] = ' '.join(td.find_all(text=True))
-        else:
-            match_obj[name] = concat_row_data(td.find_all('table')[0])
-
-    return match_obj
-
-
-def get_keonhacai(source, key_term):
-    current_time = datetime.utcnow()
-    current_timestamp = datetime.fromtimestamp(float(current_time.timestamp())).strftime('%Y-%m-%d %H:%M:%S')
-
-    # get the main table
-    soup = BeautifulSoup(source, "lxml")
-    table = soup.find("table", id="dm3")
-
-    # get all table rows at first level
-    table_rows = table.find('tbody').find_all('tr', recursive=False)
-
-    # find the row contains key term and its index
-    key_term_row = list(filter(lambda elem: len(elem.find_all('td')) == 1 and elem.text.strip() == key_term, table_rows))[0]
-    key_term_row_index = table_rows.index(key_term_row)
-
-    # find the next row the contains a term
-    next_term_row = list(filter(lambda elem: len(elem.find_all('td')) == 1, table_rows[key_term_row_index + 1:]))[0]
-    next_term_row_index = table_rows.index(next_term_row)
-
-    match_data = {'timestamp': current_timestamp, 'data': []}
-
-    for match_row in table_rows[key_term_row_index + 1: next_term_row_index]:
-        match_data['data'].append(parse_row(match_row))
-
-    return match_data
-
 
 def get_oneeighteight(source, match_name):
     soup = BeautifulSoup(source, "lxml")
